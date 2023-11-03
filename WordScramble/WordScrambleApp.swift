f7f1dd1fdeffe1ceb5db038c46b633e993b63c58
Victor Barrios <victor.barrios@bestsecret.com>
2023-11-04 00:30:20 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Barrios, Victor on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
